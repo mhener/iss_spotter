3d4c7d23207cf54b47d702bf92cabfd08446bc91
Mardin Hener <mardinhener14@gmail.com>
2022-07-30 20:00:56 +0000

    Implement promises and avoid nested cbs

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { nextISSTimesForMyLocation} = require('./iss_promises');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log('It did not work:', error.message);
+  });
